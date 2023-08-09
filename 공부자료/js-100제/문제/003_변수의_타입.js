246ed005b5f9e453b651413b99dd84bdae9263e4
SMJ426 <jerry04260@gmail.com>
2023-08-09 17:12:50 +0900

    js-알고리즘-100제-문제풀이

---
+++ 공부자료/js-100제/문제/003_변수의_타입.js
@@ -0,0 +1,23 @@
+// 다음 출력 값으로 올바른 것은?
+
+// var arr = [100, 200, 300];
+// console.log(typeof(arr));
+
+// 1)  undefined
+// 2)  string
+// 3)  number
+// 4)  object
+
+
+
+
+
+// 자바스크립트 변수의 타입
+
+// 원시 타입 : number, string, boolean, undefined, null
+
+// 참조 타입 : object
+
+
+
+// arr는 배열이기 때문에 원시타입이 아닌 참조타입이라서 object이다
