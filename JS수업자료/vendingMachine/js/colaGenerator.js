ebfdb68ca80c0361db61fb3af3fc6a446a1e0f56
SMJ426 <jerry04260@gmail.com>
2023-09-05 23:30:58 +0900

    tndjq

--- JS수업자료/vendingMachine/js/colaGenerator.js
+++ JS수업자료/vendingMachine/js/colaGenerator.js
@@ -1,51 +1,51 @@
 class ColaGenerator {
-  constructor() {
-      this.itemList = document.querySelector('.cola-list');
-  }
+    constructor() {
+        this.itemList = document.querySelector('.cola-list');
+    }
 
-  async setup() {
-      const response = await this.loadData();
-      this.colaFactory(response);
-  }
+    async setup() {
+        const response = await this.loadData();
+        this.colaFactory(response);
+    }
 
-  async loadData() {
+    async loadData() {
 
-      try {
-          const response = await fetch('./items.json');
+        try {
+            const response = await fetch('./items.json');
 
-          if (!response.ok) {
-              throw new Error('HTTP ERROR!! :' + response.status);
-          }
-          return await response.json();
-      } catch (error) {
-          console.error('콜라데이터를 로딩하는중에 문제가 발생했습니다. :' + error);
-      }
-  }
+            if (!response.ok) {
+                throw new Error('HTTP ERROR!! :' + response.status);
+            }
+            return await response.json();
+        } catch (error) {
+            console.error('콜라데이터를 로딩하는중에 문제가 발생했습니다. :' + error);
+        }
+    }
 
-  // <li>
-  //       <!-- JS : 클래스 on 유/무 제어 -->
-  //           <button type="button" class="btn-cola on">
-  //     <img src="./img/cola-original.png" alt="" />
-  //     <span class="cola-name">Original_Cola</span>
-  //     <strong class="cola-price">1000원</strong>
-  // </button>
-  //     </li>
+    // <li>
+    //       <!-- JS : 클래스 on 유/무 제어 -->
+    //           <button type="button" class="btn-cola on">
+    //     <img src="./img/cola-original.png" alt="" />
+    //     <span class="cola-name">Original_Cola</span>
+    //     <strong class="cola-price">1000원</strong>
+    // </button>
+    //     </li>
 
-  colaFactory(data) {
-      const docFrag = new DocumentFragment(); // 밑에 append를 통해 list들을 fragment에 다 모아두고 마지막에 fragment를 한번만 출력하는것 => 성능 최적화
-      data.forEach((el) => {
-          const item = document.createElement('li');
-          const itemTemplate = `<button type="button" class="btn-cola on">
-          <img src="./img/cola-original.png" alt="" />
-          <span class="cola-name">Original_Cola</span>
-          <strong class="cola-price">1000원</strong>
-        </button>`;
-          item.innerHTML = itemTemplate;
-          docFrag.append(item);
-      });
+    colaFactory(data) {
+        const docFrag = new DocumentFragment(); // 밑에 append를 통해 list들을 fragment에 다 모아두고 마지막에 fragment를 한번만 출력하는것 => 성능 최적화
+        data.forEach((el) => {
+            const item = document.createElement('li');
+            const itemTemplate = `<button type="button" class="btn-cola on data-item =${el.name} data-count=${el.count} data-price=${el.cost} data-img=${el.img}">
+            <img src="./img/${el.img}" alt="" />
+            <span class="cola-name">Original_Cola</span>
+            <strong class="cola-price">${el.cost}원</strong>
+            </button>`;
+            item.innerHTML = itemTemplate;
+            docFrag.append(item);
+        });
 
-      this.itemList.append(docFrag);
-  }
+        this.itemList.append(docFrag);
+    }
 }
 
 
--- JS수업자료/vendingMachine/js/index.js
+++ JS수업자료/vendingMachine/js/index.js
@@ -1,4 +1,8 @@
-import ColaGenerator from "./js/colaGenerator.js";
+import ColaGenerator from "./colaGenerator.js";
+import VendingMachineFunc from "./vendingMachineFunc.js";
 
 const colaGenerator = new ColaGenerator();
 colaGenerator.setup();
+
+const vendingMachine = new VendingMachineFunc();
+vendingMachine.setup();
